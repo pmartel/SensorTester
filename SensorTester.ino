b641dbee26928738fb15cc209ed38a81f86367d3
Philip Martel <pmartel@alum.mit.edu>
2017-12-16 22:43:53 -0500

    encoder and timer working
    Quadrature encoder and ballistic chronograph code are working.  The Display class is in development.

--- SensorTester.ino
+++ SensorTester.ino
@@ -24,9 +24,13 @@
   The range finder is an SR-04 type
  * trigger pin to digital pin 2
  * echo pin to digital pin 3
+
+ * Encoder / chronograph
+ * Chan A to digital pin 0
+ * Chan B to digital pin 1
  */
 
-/* TO-DO: add quadrature encoder and ballistic chronograph for velocity
+/* TO-DO: 
  *  add code to format a string into a character array and to display
  *  the array on the LCD using the UP and DOWN arrows to scroll through the
  *  array.
@@ -50,7 +54,86 @@ SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 // set up the IR receiver
 #include "IRremote.h"
 
-#define RECEIVER_PIN 6 // Signal Pin of IR receiver to Arduino Digital Pin 6
+// Signal Pin of IR receiver to Arduino Digital Pin 6
+#define RECEIVER_PIN 6 
+
+// pins for encoder or ballixtic chronograph
+// digital pins 0 and 1 interfere with the serial I/O, use the "analog" pins
+#define CHAN_A_PIN  A0
+#define CHAN_B_PIN  A1
+
+/***********************************************************/
+/* Think of having am array of characters, for now 8x16 and 
+ *  having the LCD being a window into this array.  Keys
+ *  can move the window up and down, and potentially left and right
+ */
+
+class Display {
+  public:
+    Display( int rows,int columns ){
+      _rows = rows;
+      _columns = columns;
+      _display = malloc(rows *columns); 
+    }
+    Display(void){
+      Display(8,16);
+    }
+    ~Display(){
+      free(_display);
+    }
+    Clear(){
+      int i;
+      for (i=0; i < _rows * _columns; i++){
+        _display[i] = " ";
+      }
+      Home();
+    }
+    Home(){
+      _r = 0;
+      _c = 0;
+    }
+    //load the string s starting at position (r,c)
+    // special characters handled as noted
+    // for now, wrap characters around
+    Load( char* s, int row, int col ) {
+      char c;
+      
+      _r = row;
+      _c = col;
+      // place character
+      for ( ; *s != '\0'; s++ ) {
+        c = *s;
+        switch ( c ){
+        case '\n':
+          break;
+        case '\r':
+          break;
+        case '\b':
+          break;
+        case '\t':
+          break;
+        default:
+          break;
+        }
+        // update cursor
+      }
+      
+    }
+    Load( char* s ){
+      Load( s, 0, 0);
+    }
+    
+  private:
+    // size
+    int _rows;
+    int _columns;
+    // cursor position
+    int _r;
+    int _c;
+    char* _display;
+};
+
+//Display display();
 
 /*-----( Declare objects )-----*/
 IRrecv irrecv(RECEIVER_PIN);     // create instance of 'IRrecv'
@@ -80,14 +163,18 @@ void loop() {
     case 0: // display menu
       displayMenu(0);
       break;
-    case 1:
+    case 1:  // rangefinder
       lcd.clear();
       break;
-    case 2:
-      nyi( String(mode) );
+    case 2: // quadrature encoder
+      pinMode( CHAN_A_PIN, INPUT_PULLUP );
+      pinMode( CHAN_B_PIN, INPUT_PULLUP );
+      lcd.clear();
       break;
-    case 3:
-      nyi( String(mode) );
+    case 3: //ballistic chronograph
+      pinMode( CHAN_A_PIN, INPUT);
+      pinMode( CHAN_B_PIN, INPUT);
+      lcd.clear();
       break;
     default:
       mode = 0;
@@ -96,13 +183,14 @@ void loop() {
   }
 
   switch ( mode ) { //process current mode
-  case 1:
+  case 1:  // rangefinder
     doRange(); 
     break;
-  case 2:
+  case 2: // quadrature encoder
+    doEncoder();
     break;
-  case 3:
-    break;
+  case 3: //ballistic chronograph
+    doBallistic();
   default:
     break;
   }
@@ -121,6 +209,48 @@ void displayMenu( int first ) {
   lcd << "0:menu 1:range";
   lcd.setCursor(0,1);
   lcd << "2:quad 3:speed";
+}
+
+// note: this does not return until a shot passes
+// to exit, select a different mode and then fire a shot (wave an arm, etc)
+void doBallistic( void ){
+  // wait for channel A to drop, time until channel B drops
+  long tA, tB;
+  float v;
+  String vS;
+
+#ifdef DEBUG
+  // testing with a short to ground requires pullups
+  // either in the Arduino or external
+  pinMode( CHAN_A_PIN, INPUT_PULLUP );
+  pinMode( CHAN_B_PIN, INPUT_PULLUP );
+#endif 
+ 
+  while( digitalRead( CHAN_A_PIN ) == HIGH ){};
+  tA = micros();
+  while( digitalRead( CHAN_B_PIN ) == HIGH ){};
+  tB = micros();
+  lcd.clear();
+  v = 1.e6/(tB-tA);
+  vS = String( v, 2);
+  //      0123456789012345
+  lcd << "Speed        FPS";
+  lcd.setCursor( 12 - vS.length(), 0 );
+  lcd << vS;
+}
+
+#define CHAR_L '-'
+ /* vertical bar */
+ #define CHAR_H '\x7C'
+void doEncoder( void ){
+  int chA, chB;
+
+  chA = digitalRead( CHAN_A_PIN );
+  chB = digitalRead( CHAN_B_PIN );
+  lcd.home();
+  lcd << "A: " << (chA ? CHAR_H : CHAR_L);
+  lcd.setCursor(0,1);
+  lcd << "B: " << (chB ? CHAR_H : CHAR_L);
 }
 
 void doRange( void ) {
@@ -230,10 +360,11 @@ int translateIR(void) { // takes action based on IR code received
     break;
   case 0xFF52AD: 
     Serial.println("9");
-    9;   
+    ret = 9;   
     break;
   case 0xFFFFFFFF: 
     Serial.println(" REPEAT"); // button held down
+    ret = 21;
     break;  
   default: 
     Serial.println(" other button   ");
@@ -243,4 +374,5 @@ int translateIR(void) { // takes action based on IR code received
   delay(500); // Do not get immediate repeat
   return ret;
 } //END translateIR
+
 
